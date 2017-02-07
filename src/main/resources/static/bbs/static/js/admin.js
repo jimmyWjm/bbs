0eba1a0d5bb2c46d8c816a2b0d21d28d922fe378
xiandafu <xiandafu@126.com>
2017-02-07 22:38:13 +0800

    delete js

--- src/main/resources/static/bbs/static/js/admin.js
+++
@@ -1,8 +0,0 @@
-//用户退出
-function logout() {
-    $.getJSON(getCtxPath() + "/admin/ajax/logout", function(data) {
-        setTimeout(function() {
-            location.reload(true);
-        }, 500);
-    });
-}
