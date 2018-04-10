7a578a655c6f178041b6cf09cc254d14a229b89b
ykb <1208518389@qq.com>
2018-04-10 22:48:57 +0800

    处理es搜索特殊字符报错的异常

--- src/main/java/com/ibeetl/bbs/es/service/EsService.java
+++ src/main/java/com/ibeetl/bbs/es/service/EsService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.lucene.queryparser.classic.QueryParser;
 import org.beetl.core.GroupTemplate;
 import org.beetl.core.Template;
 import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
@@ -201,6 +200,9 @@ public class EsService{
 		int pageNumber = p;
 		long pageSize = PageQuery.DEFAULT_PAGE_SIZE;
 		
+		if(keyword != null) {
+			keyword = this.string2Json(keyword);
+		}
 		PageQuery<IndexObject> pageQuery = new PageQuery<>(pageNumber, pageSize);
 		try {
 		
@@ -271,5 +273,46 @@ public class EsService{
 	
 		return pageQuery;
 	}
+	
+	/** 
+     * JSON字符串特殊字符处理
+     * @param s 
+     * @return String 
+     */  
+    public String string2Json(String s) {        
+        StringBuffer sb = new StringBuffer();        
+        for (int i=0; i<s.length(); i++) {  
+            char c = s.charAt(i);    
+             switch (c){  
+             case '\"':        
+                 sb.append("\\\"");        
+                 break;        
+             case '\\':        
+                 sb.append("\\\\");        
+                 break;        
+             case '/':        
+                 sb.append("\\/");        
+                 break;        
+             case '\b':        
+                 sb.append("\\b");        
+                 break;        
+             case '\f':        
+                 sb.append("\\f");        
+                 break;        
+             case '\n':        
+                 sb.append("\\n");        
+                 break;        
+             case '\r':        
+                 sb.append("\\r");        
+                 break;        
+             case '\t':        
+                 sb.append("\\t");        
+                 break;        
+             default:        
+                 sb.append(c);     
+             }  
+         }      
+        return sb.toString();     
+        }  
 	
 }
